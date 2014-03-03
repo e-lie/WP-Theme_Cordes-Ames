683ace86e5506d7f53093602fea739bfc2bebdb8
N4rius <eliegavoty@free.fr>
2014-03-03 17:13:31 +0100

    finition7

--- header.php
+++ header.php
@@ -130,7 +130,6 @@ echo ("<style type='text/css'>".$resulting_css.'</style>');
 
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			<h3 class="nav-toggle"><a href="#navigation">&#9776; <span><?php _e('Navigation', 'woothemes'); ?></span></a></h3>
 
 		</hgroup>
 
