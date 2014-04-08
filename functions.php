5fab0da6a0e27fa057e9e90083e99e40b6bf6434
N4rius <eliegavoty@free.fr>
2014-04-08 20:30:42 +0200

    finition33

--- functions.php
+++ functions.php
@@ -13,6 +13,11 @@
 @ini_set( 'post_max_size', '64M');
 @ini_set( 'max_execution_time', '300' );
 
+
+// Load the textdomain for translation
+load_child_theme_textdomain( 'woothemes' );
+
+
 function custom_init() {
   register_post_type( 'artist', array(
   'label' => __('Artistes'),
